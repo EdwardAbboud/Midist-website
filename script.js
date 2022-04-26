ff987c47a849a2f06246768a0d1e15af2a6ae863
jannick <jannickbath@gmail.com>
2022-04-26 19:50:40 +0200

    added navbar-toggle

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const states = ["0", "100vh"];
+let state = false;
+
 /**
  * Opens Nav-Overlay.
- * @returns {void} Changes style of DOM-Element
+ * @returns {void} Changes style of DOM-Element (navbar)
  */
 function openNav() {
   document.getElementById("myNav").style.height = "100vh";
@@ -9,11 +12,20 @@ function openNav() {
 
 /**
  * Closes Nav-Overlay.
- * @returns {void} Changes style of DOM-Element
+ * @returns {void} Changes style of DOM-Element (navbar)
  */
 function closeNav() {
   document.getElementById("myNav").style.height = "0";
   // document.querySelector(".mobile-nav").style.height = "0%";
+}
+
+/**
+ * Toggles Nav-Overlay
+ * @returns {void} Toggles style of DOM-Element (navbar)
+ */
+function toggleNav() {
+  state = !state;
+  document.getElementById("myNav").style.height = states[+ state];
 }
 
 window.onload(closeNav());
