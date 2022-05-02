273a0786d82632bef1e2e8774e0a60a49b068eef
Edward Abboud <abboudedward@gmail.com>
2022-05-02 13:49:05 +0200

    added lazy-load

--- script.js
+++ script.js
@@ -31,3 +31,20 @@ function closeNav() {
 }
 
 window.onload(closeNav());
+
+// LAZY-LOAD
+// Set the options globally
+// to make LazyLoad self-initialize
+window.lazyLoadOptions = {
+  // Your custom settings go here
+};
+
+// Listen to the initialization event
+// and get the instance of LazyLoad
+window.addEventListener(
+  "LazyLoad::Initialized",
+  function (event) {
+    window.lazyLoadInstance = event.detail.instance;
+  },
+  false
+);
